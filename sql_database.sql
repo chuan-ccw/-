e88c84cc46ea382f7bea99a916b89f80283adfc3
justin <justinliu60543@gmail.com>
2025-11-20 15:11:29 +0800

    test

--- sql_database.sql
+++ sql_database.sql
@@ -13,12 +13,12 @@ create table make
 	 primary key (order_form_id),
      foreign key (order_form_id) references order_form (order_form_id),
      foreign key (store_id) references store (store_id)
+
 	);
 
 create table order_form
 	(order_form_id		varchar(15),
 	 room_number		varchar(7),
-	 capacity		numeric(4,0),
 	 primary key (building, room_number)
 	);
 
