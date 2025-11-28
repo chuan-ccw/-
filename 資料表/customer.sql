d485112efabb5d49ea37492d19e0472536c8cb24
Ayakagril <Zongyund@gmail.com>
2025-11-28 10:10:22 +0800

    顧客資料表

---
+++ 資料表/customer.sql
@@ -0,0 +1,22 @@
+USE [DrinkShopDB]
+GO
+
+/****** Object:  Table [dbo].[Customers]    Script Date: 2025/11/28 上午 10:01:07 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Customers](
+	[CustomerID] [int] IDENTITY(1,1) NOT NULL,
+	[Phone] [nvarchar](20) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[CustomerID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
